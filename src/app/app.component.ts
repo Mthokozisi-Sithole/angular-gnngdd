5005b20cd9139edf60bdbbc5b9f9151a625067b9
Mthokozisi-Sithole <mthokozisiephraim@gmail.com>
2022-05-10 18:48:00 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-title = 'MatCurrencyFormatLibrary';
+export class AppComponent {
+  title = 'MatCurrencyFormatLibrary';
   zarAmount = 100;
-  updateUSAmount(event) {
+  updateZARAmount(event) {
     this.zarAmount = event.target.value;
   }
-
 }
